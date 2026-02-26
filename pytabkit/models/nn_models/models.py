dabf614062e928f88839b61ddc76cc6e9acba1ac
Georg Slamanig <georgslamanig@gmail.com>
2026-02-26 17:29:35 +0100

    debug prints

--- pytabkit/models/nn_models/models.py
+++ pytabkit/models/nn_models/models.py
@@ -177,7 +177,7 @@ class NNFactory(FitterFactory):
         print("==> n_cat: ", tensor_infos['x_cat'].get_n_features())
         print("==> n_num: ", tensor_infos['x_cont'].get_n_features())
         print("==> n_cat_passed: ", len(cat_cardinalities) if cat_cardinalities is not None else 'None')
-        print("==> cat_cardinalities: ", cat_cardinalities)
+        print("==> sum(cat_cardinalities): ", sum(cat_cardinalities) if cat_cardinalities is not None else 'None')
 
         # Override tensor_infos with predefined cardinalities if provided
         if cat_cardinalities is not None and 'x_cat' in tensor_infos: 
